002da2573cd6267614c5687f680bbea4886a8ef2
Christopher Riffle <cmr629@gmail.com>
2019-04-24 10:53:31 -0400

    Add to render() in TodoForms and added callbacks

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 export default class TodoForm extends React.Component {
  
+    constructor(){
+        super();
+        this.textValue = '';
+    }
+    clearButtonClicked(){
+
+    }
+
+    addButtonClicked(){
+
+    }
+
   render() {
     return (
       <div>
-         
+        <input type='text' value={this.value} className='input-box'/>
+        <button className='add-btn btn' onClick={this.addButtonClicked}>Add Todo </button>
+        <button className='clear-btn btn' onClick={this.clearButtonClicked}>Clear Completed </button>
       </div>
     );
   }
